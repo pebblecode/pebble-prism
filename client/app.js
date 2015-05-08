23efef82dfb39c72c9f8bde3f27a5eeaa4012271
Jaz Lalli <jaz@pebblecode.com>
2015-05-08 17:07:19 +0100

    colors and stuff

--- client/app.js
+++ client/app.js
@@ -5,42 +5,32 @@ var config = {
     name: 'AP 1',
     mac: '00:18:0a:ab:9f:00',
     lat: 51.485507622841,
-    lng: -0.118509843014181
+    lng: -0.118509843014181,
+    color: '#276fdc'
   }, {
     name: 'AP 2',
     mac: '00:18:0a:ab:9e:c0',
     lat: 51.4854758881698,
-    lng: -0.118493749760091
+    lng: -0.118493749760091,
+    color: '#e22d00'
   }, {
     name: 'AP 3',
     mac: '00:18:0a:ab:a8:80',
     lat: 51.4854365068737,
-    lng: -0.11845531873405
+    lng: -0.11845531873405,
+    color: '#00aedb'
   }, {
     name: 'AP 4',
     mac: '00:18:0a:aa:d0:a0',
     lat: 51.4854391427336,
-    lng: -0.118544711731374
+    lng: -0.118544711731374,
+    color: '#f4ce00'
   }, {
     name: 'AP 5',
     mac: '00:18:0a:ac:31:00',
     lat: 51.4854037151986,
-    lng: -0.118526376318187
-  }]
-};
-
-var points = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-0.11851372534988514, 51.485449670914605]
-    },
-    properties: {
-      title: 'asdf',
-      radius: 0.1
-    }
+    lng: -0.118526376318187,
+    color: '#1bb610'
   }]
 };
 
@@ -51,7 +41,7 @@ var map = L.mapbox.map('map', 'eyko.m4g58njk');
 var apLayerGroup = L.layerGroup(_.map(config.accessPoints, function (ap) {
   var marker = L.marker([ap.lat, ap.lng]);
   marker.setIcon(L.mapbox.marker.icon({
-    'marker-color': '#ff0000',
+    'marker-color': ap.color,
     'marker-size': 'small',
     'stroke-width': 2
   }));
@@ -63,8 +53,8 @@ apLayerGroup.addTo(map);
 
 
 // events
-//
 
+var devices = {};
 var devicesLayerGroup = L.layerGroup(null);
 
 var es = new EventSource('http://pebble-prism.herokuapp.com/subscribe');
@@ -72,11 +62,33 @@ es.onmessage = function (e) {
   devicesLayerGroup.clearLayers();
 
   var msg = JSON.parse(e.data);
+  var apMac = msg.data.accessPointMac;
 
-  _.each(msg.data.deviceCoordinates, function (coord) {
-    var marker = L.circleMarker([coord.lat, coord.lng], {radius: coord.unc}).addTo(map);
-    devicesLayerGroup.addLayer(marker);
+  devices[apMac] = _.map(msg.data.deviceCoordinates, function (coord) {
+    var marker = L.circleMarker([coord.lat, coord.lng], {
+      radius: coord.unc,
+      fill: true,
+      fillColor: config.accessPoints.filter(function (item) { return item.mac === apMac; })[0].color,
+      fillOpacity: 0.3,
+      stroke: true,
+      color: config.accessPoints.filter(function (item) { return item.mac === apMac; })[0].color,
+      opacity: 0.8
+    });
+
+    marker.setStyle(L.mapbox.marker.icon({
+
+    }));
+
+    return marker;
   });
+
+  for (var key in devices) {
+    if (devices.hasOwnProperty(key)) {
+      devices[key].forEach(function (item) {
+        devicesLayerGroup.addLayer(item);
+      })
+    }
+  }
 };
 
 devicesLayerGroup.addTo(map);
